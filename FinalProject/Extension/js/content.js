20b61b8b7caa1394803e4390a8fefeaae899a00c
Arjun Akkiraju <20arjuna@students.harker.org>
2019-05-20 14:03:28 -0700

    gittaWOOONK

---
+++ FinalProject/Extension/js/content.js
@@ -0,0 +1,9 @@
+var site = "";
+chrome.tabs.getSelected(null,function(tab) {
+    var tablink = new URL(tab.url);
+    site = tablink.hostname;
+    document.getElementById("sitename").innerHTML = site + " may be trying to pull a fast one on you!";
+
+
+
+});
