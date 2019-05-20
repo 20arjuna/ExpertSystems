7a7f67a74f7363bdd7e03c9b010f35774be3b039
Arjun Akkiraju <20arjuna@students.harker.org>
2019-05-20 15:39:50 -0700

    sigh

--- FinalProject/Extension/js/popup.js
+++ FinalProject/Extension/js/popup.js
@@ -1,9 +1,7 @@
 var site = "";
+console.log("bitch");
 chrome.tabs.getSelected(null,function(tab) {
     var tablink = new URL(tab.url);
     site = tablink.hostname;
     document.getElementById("sitename").innerHTML = site + " may be trying to pull a fast one on you!";
-
-
-
 });
