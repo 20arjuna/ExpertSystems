171478bbe2de585f3bd0edc178313515bf5e8c73
Arjun Akkiraju <20arjuna@students.harker.org>
2019-05-22 11:48:54 -0700

    task list

---
+++ FinalProject/tasklist.py
@@ -0,0 +1,19 @@
+    #Sudo code
+
+  python.writeToCSV(characteristicList, 'characteristics.csv')
+  python.callJava('callJess.java')
+
+  #JAVA psuedo code
+  java.assert(dataFromCSV)
+  java.callJESS()
+
+  #JESS psuedo code
+  JESS.determineOutput()
+  JESS.sendOutputToJava(output)
+
+  #JAVA psuedo code
+  java.writeToCSV(output)
+
+  #python psuedo code
+  python.readFromCSV()
+  python.render_template('new HTML')
