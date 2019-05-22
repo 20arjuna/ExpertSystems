3c6108ffc70ce91e199dddac56a3920fc5453881
Arjun Akkiraju <20arjuna@students.harker.org>
2019-05-22 14:26:23 -0700

    java will def not work

--- FinalProject/analyze.py
+++ FinalProject/analyze.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template, Response, request, redirect, url_for
-from googlesearch import search
 import csv
+from subprocess import STDOUT, PIPE
+import os.path
 
 app = Flask(__name__)
 
@@ -20,13 +21,25 @@ def analyze():
                 for element in row:
                     characteristicList.append(row[element])
     characteristicList.pop(0)
-    characteristicList = [site] + characteristicList
-    #Writing list to csv
-    with open('static/characteristics.csv', mode='w') as char_file:
-        char_file = csv.writer(char_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        char_file.writerow(characteristicList)
+    # characteristicList = [site] + characteristicList
+    # #Writing list to csv
+    # with open('static/characteristics.csv', mode='w') as char_file:
+    #     char_file = csv.writer(char_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    #     char_file.writerow(characteristicList)
 
+    compile_java('test.java')
+    execute_java('test.java', 5)
     return site
+
+def compile_java(java_file):
+    subprocess.check_call(['javac', java_file])
+
+def execute_java(java_file, stdin):
+    java_class,ext = os.path.splitext(java_file)
+    cmd = ['java', java_class]
+    proc = subprocess.Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+    stdout,stderr = proc.communicate(stdin)
+    print ('This was "' + stdout + '"')
 
 if __name__ == "__main__":
     app.run()
---
+++ FinalProject/test.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class test
+{
+  public static void main (String [] args)
+  {
+    Scanner sc = new Scanner (System.in);
+    int var = sc.nextLine();
+    System.out.println("Java says: " + var);
+  }
+}
